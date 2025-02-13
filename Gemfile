4d844e2673882630ea4bc880d43cceb88cdc61f8
Alexander Panov <panov@royalzsoftware.de>
2025-02-14 00:02:31 +0100

    routes

--- Gemfile
+++ Gemfile
@@ -4,12 +4,8 @@ source "https://rubygems.org"
 
 # gem "rails"
 
-gem "rack", "~> 3.1"
-
-gem "hanami-router", "~> 0.6.2"
-
-gem "rackup", "~> 2.2"
-
 gem "activerecord", "~> 8.0"
 
 gem "sqlite3", "~> 2.5"
+
+gem "sinatra", "~> 4.1"
--- web/router.rb
+++ web/router.rb
@@ -1,9 +1,60 @@
+require_relative '../lib/whitelabeling_collector.rb';
+require 'active_record'
 require 'json'
-require 'hanami/router'
-require 'rack'
-require 'rackup'
+require 'sinatra'
 
-module WhitelabelingCollector
-  module Web
+WhitelabelingCollector::Lib.init(adapter: 'sqlite3', database: 'Test.sqlite3')
+before '/*' do
+  content_type :json
+end
+
+post '/organizations' do
+end
+
+def validate_user_is_organization_admin
+  if params[:organization_id] == "69"
+    halt 401
   end
+  # todo
+end
+
+get '/organizations/:organization_id/data_buckets' do
+  validate_user_is_organization_admin
+
+  WhitelabelingCollector::Lib::DataBucket.where(organization_id: params[:organization_id]).to_json
+end
+
+get '/organizations/:organization_id/data_buckets/:data_bucket_id' do
+  validate_user_is_organization_admin
+
+  WhitelabelingCollector::Lib::DataBucket.find(params[:data_bucket_id]).to_json
+end
+
+def json_body
+  JSON.parse request.body.read
+end
+
+post '/organizations/:organization_id/data_buckets' do
+  validate_user_is_organization_admin
+
+  WhitelabelingCollector::Lib::DataBucket.create(organization_id: params[:organization_id], name: json_body['name']).to_json
+end
+
+get '/organizations/:organization_id/data_buckets/:data_bucket_id/submissions' do
+  validate_user_is_organization_admin
+end
+
+post '/organizations/:organization_id/data_buckets/:data_bucket_id/submissions' do
+  validate_user_is_organization_admin
+  WhitelabelingCollector::Lib::Submission.create(organization_id: params[:organization_id], data_bucket_id: params[:data_bucket_id])
+end
+
+get '/organizations/:organization_id/data_buckets/:data_bucket_id/submissions/:submission_id' do
+end
+
+put '/organizations/:organization_id/data_buckets/:data_bucket_id/submissions/:submission_id' do
+end
+
+post '/organizations/:organization_id/data_buckets/:data_bucket_id/submissions/:submission_id/validate' do
+  validate_user_is_organization_admin
 end
